537d0f85e941804bcc036dd9a603b8c5701c8257
amirkian007 <amircc1200@gmail.com>
2023-03-23 06:24:25 -0400

    chore(ircolor-uikit):MOVE icons import to uikit insted from playgroud

--- packages/ircolor-uikit/playground/main.ts
+++ packages/ircolor-uikit/playground/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // import './assets/style/main.css'
-import 'ircolor-icons/pack/css/ircolor-icons.css'
 
 import * as uipack from "../src/entry-bundler"
 
