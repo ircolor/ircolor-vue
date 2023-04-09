50359c0e3363006a8df1de34424eb8ae037b40dd
Shahriar Eshraghi <shahriareshraghi22@gmail.com>
2023-04-09 12:01:45 +0330

    add config into VButton story

--- apps/docs/src/components/VButton.story.vue
+++ apps/docs/src/components/VButton.story.vue
@@ -1,34 +1,50 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import { logEvent } from 'histoire/client'
 
-const label = ref('Click me')
-const variant = ref('danger')
-const type = ref('outline')
-const iconPosition = ref('')
-const icon = ref('')
-const rounded = ref('')
-const disabled = ref(false)
-const hasIcon = ref(false)
+const state = reactive({
+  label: 'متن تست',
+  variant: 'primary',
+  rounded: '2xl',
+  type: 'fill',
+  iconPosition: 'left',
+  hasIcon: false,
+  disabled: false
+})
+// const label = ref('Click me')
+// const variant = ref('danger')
+// const type = ref('outline')
+// const iconPosition = ref('')
+// const icon = ref('')
+// const rounded = ref('')
+// const disabled = ref(false)
+// const hasIcon = ref(false)
 </script>
 <template>
-  <Story title="Button">
-    <Variant title="number field" auto-props-disabled>
+  <Story title="VButton" :layout="{ type: 'single', iframe: true }">
+    <Variant title="default" :state="state">
       <template #default="{ state }">
         <v-button
-          :label="label"
-          :type="type"
-          :variant="variant"
-          :iconPosition="iconPosition"
-          :icon="icon"
-          :rounded="rounded"
-          :disabled="disabled"
-          :hasIcon="hasIcon"
+          :label="state.label"
+          :type="state.type"
+          :variant="state.variant"
+          :iconPosition="state.iconPosition"
+          :rounded="state.rounded"
+          :disabled="state.disabled"
+          :hasIcon="state.hasIcon"
           @click="logEvent('click', $event)"
         >
+          <template #icon>
+            <TestIcon />
+          </template>
         </v-button>
       </template>
-      <template #controls="{ state }">
+
+      <template #controls>
+        Content: <input type="text" v-model="state.label" />
+        Disabled: <input type="checkbox" v-model="state.disabled" />
+      </template>
+      <!-- <template #controls="{ state }">
         <HstText v-model="state.label" title="label" />
         <HstSelect
           v-model="state.type"
@@ -69,7 +85,7 @@ const hasIcon = ref(false)
         />
         <HstCheckbox v-model="state.disabled" title="disabled" />
         <HstCheckbox v-model="state.hasIcon" title="hasIcon" />
-      </template>
+      </template> -->
     </Variant>
   </Story>
 </template>
